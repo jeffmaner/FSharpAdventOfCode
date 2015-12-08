80f84dca02ec733a11becab6aa31209bf7b8ca9b
Jeff Maner <jeffmaner@yahoo.com>
2015-12-08 09:47:45 -0600

    Finish Day 5, Part 2; refine Part 1.

--- 05.fsx
+++ 05.fsx
@@ -2,12 +2,21 @@ module Day05
 
 open System.IO
 
-let inline all    xs = List.forall xs
-let inline filter xs = Seq.filter  xs
-let inline length xs = Seq.length  xs
-let inline map    xs = Seq.map     xs
-let inline toList xs = List.ofSeq  xs
-let inline toSeq  xs = List.toSeq  xs
+let inline all      xs = List.forall  xs
+let inline contains xs = Seq.contains xs
+let inline exists   xs = List.exists  xs
+let inline filter   xs = Seq.filter   xs
+let inline first    xs = Array.head   xs
+let inline groupBy  xs = Seq.groupBy  xs
+let inline indexed  xs = Seq.indexed  xs
+let inline last     xs = Array.last   xs
+let inline length   xs = Seq.length   xs
+let inline map      xs = Seq.map      xs
+let inline pairwise xs = Seq.pairwise xs
+let inline reduce   xs = Seq.reduce   xs
+let inline toList   xs = List.ofSeq   xs
+let inline toSeq    xs = List.toSeq   xs
+let inline windowed xs = Seq.windowed xs
 
 // A la Haskell.
 let (</>) p q = Path.Combine (p,q)
@@ -26,7 +35,7 @@ let rec isPrefixOf needle haystack =
     | (x::xs,y::ys) -> x=y && isPrefixOf xs ys
 
 // A la Haskell.
-let rec isInfixOf needle haystack = List.exists (isPrefixOf needle) (tails haystack)
+let rec isInfixOf needle haystack = exists (isPrefixOf needle) (tails haystack)
 
 // A la Haskell.
 let flip f x y = f y x
@@ -34,10 +43,10 @@ let flip f x y = f y x
 // Part One.
 let isNice s =
     let vowels = [ 'a'; 'e'; 'i'; 'o'; 'u' ] |> toSeq
-    let vowelCount = filter (fun c -> Seq.contains c vowels) >> length
+    let vowelCount = filter (fun c -> contains c vowels) >> length
     let atLeastThreeVowels = vowelCount >> (<=) 3
 
-    let repeatedCharCount = Seq.pairwise >> filter (fun (x,y) -> x=y) >> length
+    let repeatedCharCount = pairwise >> filter (fun (x,y) -> x=y) >> length
     let atLeastOneRepeatedChar = repeatedCharCount >> (<) 0
 
     let offendingCombinations = [ "ab"; "cd"; "pq"; "xy" ] |> toSeq |> map toList
@@ -55,5 +64,35 @@ let niceStrings =
   |> File.ReadAllLines
   |> filter isNice
   |> length // 258
+
+// Part Two.
+let isNice' s =
+    let containsRepeatedSequences s' =
+        let groups = s' |> (windowed 2 >> indexed >> groupBy snd)
+
+        let repeated = snd >> length >> (<) 1
+        let filterRepeats = filter repeated
+        let stripChars = map fst
+        let simplify = map (fun g -> fst g, snd g |> stripChars)
+        let sortIndices = map (fun g -> fst g, snd g |> Seq.sortDescending)
+        let measureDistances = map (fun (k,vs) -> (k, vs |> reduce (-)))
+
+        let distances = groups |> (filter repeated >> simplify >> sortIndices >> measureDistances)
+        let repeatedSequences = distances |> filter (snd >> (<) 1) |> map fst
+         in repeatedSequences |> length |> (<) 0
+
+    let containsPattern s' =
+        let matchesPattern a = first a = last a
+        let sequences = s' |> (windowed 3 >> filter matchesPattern)
+         in sequences |> (length >> (<) 0)
+
+    let requirements = [containsRepeatedSequences; containsPattern]
+     in all (fun f -> f s) requirements
+
+let niceStrings' =
+  __SOURCE_DIRECTORY__ </> "05.input"
+  |> File.ReadAllLines
+  |> filter isNice'
+  |> length // 53
 
 // vim:ft=fs
