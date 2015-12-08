e6e478bc8905d7a8f29410a69e1318a7e1dfdf8a
Jeff Maner <jeffmaner@yahoo.com>
2015-12-07 21:24:45 -0600

    Turns out Value Restriction isn't a problem when compiled inside isNice.

--- 05.fsx
+++ 05.fsx
@@ -34,18 +34,18 @@ let flip f x y = f y x
 // Part One.
 let isNice s =
     let vowels = [ 'a'; 'e'; 'i'; 'o'; 'u' ] |> toSeq
-    let vowelCount : string -> int = filter (fun c -> Seq.contains c vowels) >> length
-    let atLeastThreeVowels : string -> bool = vowelCount >> (<=) 3
+    let vowelCount = filter (fun c -> Seq.contains c vowels) >> length
+    let atLeastThreeVowels = vowelCount >> (<=) 3
 
-    let repeatedCharCount : string -> int = Seq.pairwise >> filter (fun (x,y) -> x=y) >> length
-    let atLeastOneRepeatedChar : string -> bool = repeatedCharCount >> (<) 0
+    let repeatedCharCount = Seq.pairwise >> filter (fun (x,y) -> x=y) >> length
+    let atLeastOneRepeatedChar = repeatedCharCount >> (<) 0
 
     let offendingCombinations = [ "ab"; "cd"; "pq"; "xy" ] |> toSeq |> map toList
     let offenses s =
         let s' = toList s
          in filter (flip isInfixOf s') offendingCombinations
-    let isOffensive : string -> bool = offenses >> length >> (<) 0
-    let isNotOffensive : string -> bool = not << isOffensive
+    let isOffensive = offenses >> length >> (<) 0
+    let isNotOffensive = not << isOffensive
 
     let requirements = [ atLeastThreeVowels; atLeastOneRepeatedChar; isNotOffensive ]
      in all (fun f -> f s) requirements
@@ -55,11 +55,5 @@ let niceStrings =
   |> File.ReadAllLines
   |> filter isNice
   |> length // 258
-
-// This FS0030 Value Restriction error is seriously pissing me off. What's the
-// point in point-free programming if we can't actually do point-free
-// programming?
-// First I just added explicit parameters. That grated on me. So I replaced
-// explicit parameters with type function signatures.
 
 // vim:ft=fs
