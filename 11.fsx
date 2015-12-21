f824fd7cddeaee975bc1732b46504fc8325178d1
Jeff Maner <jeffmaner@yahoo.com>
2015-12-21 08:17:25 -0600

    Day 11 solution.

--- 11.fsx
+++ 11.fsx
@@ -3,6 +3,10 @@ module Day11
 #load "ancillary.fsx"
 open Ancillary
 
+let inline every f xs = Seq.forall   f xs
+
+let join (cs:char list) = System.String.Join("", cs)
+
 let inc = function
     | 'z' -> (Some 'a','a')
     |  c  -> (None, int c |> (+) 1 |> char)
@@ -17,7 +21,7 @@ let increment chars =
         | x::xs -> match inc x with
                    | (Some _, d) -> d::f xs
                    | (None  , c) -> c::xs
-     in chars |> (List.rev >> f >> List.rev)
+     in chars |> (rev >> f >> rev)
 
 let incrementPassword = increment
 
@@ -26,26 +30,24 @@ let includesOneIncreasingStraightOfThree (chars: char list) =
     let differences = Seq.pairwise >> map (fun t -> snd t - fst t)
     let trues = filter (id)
     let isOne = (=) 1
-    let inline every f xs = Seq.forall f xs
     let isIncreasing = charsToInts >> differences >> every isOne
-    let moreThanOne = trues >> length >> ((<) 1)
-     in chars |> (List.windowed 3 >> map isIncreasing >> moreThanOne)
+    let moreThanOne = trues >> length >> ((<=) 1)
+     in chars |> (windowed 3 >> map isIncreasing >> moreThanOne)
 
 let doesNotIncludeConfusingLetters chars =
     let confusingLetters = charList "ilo"
     let isConfusingLetter c = c |> elem <| confusingLetters
      in chars |> (map isConfusingLetter >> filter (id) >> length >> ((=) 0))
 
-// Move windowed to Ancillary and refactor fsxs.
-let inline windowed n xs = Seq.windowed n xs
-
 let containsAtLeastTwoDifferentNonOverlappingPairs chars =
-    let pairs = chars |> windowed 2 |> filter (fun xs -> xs.[0] = xs.[1]) |> filter (fun xs -> length xs |> ((=) 2))
+    let atLeastTwo = length >> ((<=) 2)
+    let isPair xs = 2 = length xs
+    let pairs = chars |> (windowed 2 >> filter (fun xs -> xs.[0] = xs.[1]) >> filter isPair)
     let distinctPairs = pairs |> Seq.distinct
-     in distinctPairs |> (length >> ((<=) 2))
+     in distinctPairs |> atLeastTwo
 
 let isValid s =
-    let requirements = [ doesNotIncludeConfusingLetters; includesOneIncreasingStraightOfThree; containsAtLeastTwoDifferentNonOverlappingPairs ]
+    let requirements = [ doesNotIncludeConfusingLetters; containsAtLeastTwoDifferentNonOverlappingPairs; includesOneIncreasingStraightOfThree ]
      in all (fun f -> f s) requirements
 
 let rec until test f x =
@@ -54,11 +56,9 @@ let rec until test f x =
         then x'
         else until test f x'
 
-// Move join to Ancillary and refactor fsxs.
-let join (cs:char list) = System.String.Join("", cs)
+let currentPassword =  "hxbxwxba"
 
-let currentPassword =  "hxbxwxba" // "ghijklmn" // "abcdefgh"
-
-let partOne = until isValid incrementPassword (charList currentPassword) |> join // hxccdeff is incorrect.
+let partOne = until isValid incrementPassword (charList currentPassword) |> join // hxbxxyzz
+let partTwo = until isValid incrementPassword (charList partOne) |> join // hxcaabcc
 
 // vim:ft=fs
