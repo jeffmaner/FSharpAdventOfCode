4a5b733d3800fa51a82e1ab4469de24f1c379c19
Jeff Maner <jeffmaner@yahoo.com>
2015-12-23 18:46:07 -0600

    Day 12, Part Two.

--- 12.fsx
+++ 12.fsx
@@ -13,4 +13,21 @@ let ns = [ for n in numbers do
 
 let partOne = sum ns // 156366
 
+#load "json-parser.fsx"
+open JsonParser
+
+let (=~) s t = Regex.IsMatch(s,t)
+
+let isRedObject = function
+    | JSString s -> s =~ ".*red.*" // Apparently s won't equal red, "red", ""red"", or anything.
+    | _          -> false
+
+let rec removeRedObjects = function
+    | JSObject o -> if Map.exists (fun k v -> isRedObject v) o then [0] else Map.toList o |> List.collect (fun (k,v) -> removeRedObjects v)
+    | JSNumber n -> [int n]
+    | JSArray  a -> List.collect removeRedObjects a
+    | _          -> []
+
+let partTwo = json().Parse input |> removeRedObjects |> sum // 96852
+
 // vim:ft=fs
